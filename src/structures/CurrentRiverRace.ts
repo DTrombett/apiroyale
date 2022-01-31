d8659005fb4725228c3d8497eaa79d2d5cd937f4
DTrombett <maxtromb.dt@gmail.com>
2022-01-31 15:21:29 +0100

    fix(CurrentRiverRace): wrong week day

--- src/structures/CurrentRiverRace.ts
+++ src/structures/CurrentRiverRace.ts
@@ -77,7 +77,7 @@ export class CurrentRiverRace<
 		this.monthDay = data.periodIndex + 1;
 		this.state = data.state;
 		this.type = data.periodType;
-		this.day = data.sectionIndex || (this.monthDay - 3) % 7;
+		this.day = data.sectionIndex + 1;
 	}
 
 	/**
